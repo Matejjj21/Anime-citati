51b304bf48d68d52bf21d3290ec2dfd5baaa45f8
Matej Dragičević <100135264+Matejjj21@users.noreply.github.com>
2022-09-13 10:23:58 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <v-app>
+      <NavBar />
+
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar";
+
+export default {
+  components: { NavBar },
+  name: "App"
+};
+</script>
+
+<style>
+* {
+  
+  
+  color: red;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  
+}
+
+a {
+  all: unset;
+}
+</style>
